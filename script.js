71c6ababeaa9541742b01cb6317d35dfbd0e107e
Nurali_NM3 <nurdtinovnurali@gmail.com>
2023-03-06 15:12:20 +0600

    first homework

---
+++ script.js
@@ -0,0 +1,37 @@
+// let a = null
+// console.log(a)
+// let a = 55
+// console.log (a.toString())
+// let a = 'str'
+// let b = 10
+// console.log(a === b)
+// let a = null
+// console.log(a)
+// let a = 15
+// let v = 18
+// console.log(a + v)
+// let a = 24
+// let n = "2"
+// console.log(24 / Number(2))
+
+// let a = 4 
+// let b = 55
+// console.log(a + b)
+// console.log(a * b)
+// console.log(b / a)
+// console.log(a % b)
+// let a = 'hello '
+// let b = 'everyone'
+// let n = 'Nurali'
+// let c = 3
+// console.log(a + b)
+// console.log(a + n)
+// console.log(a * c)
+// let a = 5
+// let b = '5'
+// let c = Number(b)
+// console.log(a + c)
+// let c = 2
+// let b = undefined
+// console.log(typeof c)
+// console.log(typeof b)
